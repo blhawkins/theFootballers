8676112efa5c99027c1ecaffb8a8446c2b9ef579
Benjamin Hawkins <66797762+blhawkins@users.noreply.github.com>
2020-11-16 21:16:17 -0600

    Working!

---
+++ Static/JS/dataFiltering.js
@@ -0,0 +1,94 @@
+
+function initializeTable() {
+    d3.json("../Data/scraped_data.json", function(data) {
+        //Table creation
+        var playerData = data[0]['players_data'];
+        playerData.forEach(tablePopulationFun)
+    });
+};
+
+function tablePopulationFun(player) {
+    var tbody = d3.select('tbody') //NOTE the data.html file must have this
+    var row = tbody.append('tr')
+    Object.entries(player).forEach(function([key, value]) {
+        var cell = row.append('td')
+        cell.text(value)
+    });
+}
+
+function nameFilter(dt) {
+    let inputSearchName = d3.select("#name"); //MAKE SURE YOU HAVE THIS
+    let inputSearchNameValue = inputSearchName.property("value");
+    if (inputSearchNameValue)
+        return dt.name == inputSearchNameValue //Figure out how to return 'includes this'
+    return true;
+}
+
+function positionFilter(dt) {
+    let inputSearchPosition = d3.select("#position"); //MAKE SURE YOU HAVE THIS
+    let inputSearchPositionValue = inputSearchPosition.property("value");
+    if (inputSearchPositionValue)
+        return dt.primary_position == inputSearchPositionValue //Figure out how to return 'includes this'
+    return true;
+}
+
+function ageFilter(dt) {
+    let inputSearchAge = d3.select("#age"); //MAKE SURE YOU HAVE THIS
+    let inputSearchAgeValue = inputSearchAge.property("value");
+    if (inputSearchAgeValue)
+        return dt.age == inputSearchAgeValue //Figure out how to return 'includes this'
+    return true;
+}
+
+function overallRatingFilter(dt) {
+    let inputSearchOverall = d3.select("#overall_rating"); //MAKE SURE YOU HAVE THIS
+    let inputSearchOverallValue = inputSearchOverall.property("value");
+    if (inputSearchOverallValue)
+        return dt.overall_rating == inputSearchOverallValue //Figure out how to return 'includes this'
+    return true;
+}
+
+function potentialRatingFilter(dt) {
+    let inputSearchPotential = d3.select("#potential_rating"); //MAKE SURE YOU HAVE THIS
+    let inputSearchPotentialValue = inputSearchPotential.property("value");
+    if (inputSearchPotentialValue)
+        return dt.potential_rating == inputSearchPotentialValue //Figure out how to return 'includes this'
+    return true;
+}
+
+function countryFilter(dt) {
+    let inputSearchCountry = d3.select("#country"); //MAKE SURE YOU HAVE THIS
+    let inputSearchCountryValue = inputSearchCountry.property("value");
+    if (inputSearchCountryValue)
+        return dt.national_team == inputSearchCountryValue //Figure out how to return 'includes this'
+    return true;
+}
+
+function playerFiltering() {
+    //Pull in data
+    d3.json("../Data/scraped_data.json", function(data) {
+        //Table creation
+        var playerData = data[0]['players_data'];
+        // Prevent Page Refresh
+        //d3.event.preventDefault(); //WHY IS THIS NOT WORKING??
+        // Remove Table Body
+        d3.select("tbody").selectAll("tr").remove();
+        // Filter Table based on selections
+        let playerFilteredData = playerData.filter(nameFilter);
+        playerFilteredData = playerData.filter(positionFilter);
+        playerFilteredData = playerData.filter(ageFilter);
+        playerFilteredData = playerData.filter(overallRatingFilter);
+        playerFilteredData = playerData.filter(potentialRatingFilter);
+        playerFilteredData = playerData.filter(countryFilter);
+        // Reload Filtered Table
+        playerFilteredData.forEach(tablePopulationFun)
+    });
+}
+
+
+let filterButton = d3.select("#filter-btn");
+// console.log(document.getElementById("filter-btn"));
+// console.log(filterButton);
+filterButton.on("click", playerFiltering);
+
+initializeTable()
