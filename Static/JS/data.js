d51e9e948e961fe71beb7bb09a345e819a95a5f5
Benjamin Hawkins <66797762+blhawkins@users.noreply.github.com>
2020-11-30 14:50:33 -0600

    Added toLowerCase()

--- Static/JS/data.js
+++ Static/JS/data.js
@@ -34,48 +34,49 @@ function nameFilter(dt) {
     let inputSearchName = d3.select("#name"); //MAKE SURE YOU HAVE THIS
     let inputSearchNameValue = inputSearchName.property("value");
     if (inputSearchNameValue)
-        return dt.name.includes(inputSearchNameValue);
+        return dt.name.toLowerCase().includes(inputSearchNameValue.toLowerCase());
         //https://stackoverflow.com/questions/47785624/filter-and-includes-to-filter-array-with-array
+        //https://www.google.com/search?q=How+to+change+to+lowercase+in+Javascript&oq=How+to+change+to+lowercase+in+Javascript&aqs=chrome..69i57.9364j0j7&sourceid=chrome&ie=UTF-8
     return true;
 }
 
 function positionFilter(dt) {
-    let inputSearchPosition = d3.select("#position"); //MAKE SURE YOU HAVE THIS
+    let inputSearchPosition = d3.select("#position");
     let inputSearchPositionValue = inputSearchPosition.property("value");
     if (inputSearchPositionValue)
-        return dt.primary_position == inputSearchPositionValue //Figure out how to return 'includes this'
+        return dt.primary_position.toLowerCase() == inputSearchPositionValue.toLowerCase()
     return true;
 }
 
 function ageFilter(dt) {
-    let inputSearchAge = d3.select("#age"); //MAKE SURE YOU HAVE THIS
+    let inputSearchAge = d3.select("#age");
     let inputSearchAgeValue = inputSearchAge.property("value");
     if (inputSearchAgeValue)
-        return dt.age == inputSearchAgeValue //Figure out how to return 'includes this'
+        return dt.age == inputSearchAgeValue
     return true;
 }
 
 function overallRatingFilter(dt) {
-    let inputSearchOverall = d3.select("#overall_rating"); //MAKE SURE YOU HAVE THIS
+    let inputSearchOverall = d3.select("#overall_rating");
     let inputSearchOverallValue = inputSearchOverall.property("value");
     if (inputSearchOverallValue)
-        return dt.overall_rating == inputSearchOverallValue //Figure out how to return 'includes this'
+        return dt.overall_rating == inputSearchOverallValue
     return true;
 }
 
 function potentialRatingFilter(dt) {
-    let inputSearchPotential = d3.select("#potential_rating"); //MAKE SURE YOU HAVE THIS
+    let inputSearchPotential = d3.select("#potential_rating");
     let inputSearchPotentialValue = inputSearchPotential.property("value");
     if (inputSearchPotentialValue)
-        return dt.potential_rating == inputSearchPotentialValue //Figure out how to return 'includes this'
+        return dt.potential_rating == inputSearchPotentialValue
     return true;
 }
 
 function countryFilter(dt) {
-    let inputSearchCountry = d3.select("#country"); //MAKE SURE YOU HAVE THIS
+    let inputSearchCountry = d3.select("#country");
     let inputSearchCountryValue = inputSearchCountry.property("value");
     if (inputSearchCountryValue)
-        return dt.national_team.includes(inputSearchCountryValue)
+        return dt.national_team.toLowerCase().includes(inputSearchCountryValue.toLowerCase())
     return true;
 }
 
